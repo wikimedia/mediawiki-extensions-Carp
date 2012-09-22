ef4f0c45c701441ae61e836cdc4e168881252367
Raimond Spekking <raimond.spekking@gmail.com>
2012-09-22 07:39:17 +0000

    Revert "Localisation updates from http://translatewiki.net."
    TWN export script broke file due to some style error in the i18n file
    
    This reverts commit c811415975fb9ecfe52b2032c0ae6726be6c0bd0

--- Carp.i18n.php
+++ Carp.i18n.php
@@ -6,8 +6,19 @@
  * @ingroup Extensions
  */
 
-$messages = array();/** Message documentation (Message documentation) */
-$messages['qqq'] = array(
+$messages = array();
+
+/** English
+ * @author Van de Bugger
+ */
+$messages[ 'en' ] = array(
+	'carp-desc' => 'Provides error reporting and stack unwinding to other extensions.',
+	'carp-function-msg' => '&#123;&#123; $1: $2 &#125;&#125;',
+	'carp-template-msg' => '&#123;&#123; $1 &#124; $2 &#125;&#125;',
+	'carp-template'     => '&#123;&#123; $1 &#125;&#125;',
+);
+
+$messages[ 'qqq' ] = array(
 	'carp-function-msg' => 'Parser function error decorator. Paramaters:
 $1 is name of function,
 $2 is the error message.
@@ -20,25 +31,12 @@ Translators can change order (e. g. put function name on the left side of error 
 $1 is template name,
 $2 is error message.
 Similar to parser function decorator, but this one formats template errors.',
-	'carp-template' => 'Template decorator. Used in stack backtrace.',
+	'carp-template'     => 'Template decorator. Used in stack backtrace.',
 );
 
-/** German (Deutsch)
- * @author Kghbln
+/** Russian (Русский)
+ * @author Van de Bugger
  */
-$messages['de'] = array(
-	'carp-desc' => 'Ermöglicht Fehlerberichterstattungen und Stapelabwicklungen zu anderen Erweiterungen',
-);
-
-/** Upper Sorbian (hornjoserbsce)
- * @author Michawiki
- */
-$messages['hsb'] = array(
-	'carp-desc' => 'Zmóžnja zdźělenje zmylkow a wotwiwanje staplow k druhim rozšěrjenjam.',
-);
-
-/** Russian (русский) */
-$messages['ru'] = array(
+$messages[ 'ru' ] = array(
 	'carp-desc' => 'Предоставляет другим расширениям сервис для сообщений об ошибках и раскрутки стека вызовов.',
 );
-
